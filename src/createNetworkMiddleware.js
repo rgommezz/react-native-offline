ed45cf230e4fd908adb3e8cf2f9ff9ea444ea64c
Rauliyohmc <rauliyohmc@gmail.com>
2017-03-27 22:04:58 +0200

    adding redux middleware for handling network calls in offline mode

---
+++ src/createNetworkMiddleware.js
@@ -0,0 +1,25 @@
+import { FETCH_OFFLINE_MODE } from './reducer';
+
+function createNetworkMiddleware({ regexActionType = /FETCH.*REQUEST/, regexActionName = /fetch/, actionTypes = [] }) {
+  return ({ getState }) => next => (action) => {
+    const connectionState = getState().network.isConnected;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+    // Variables and methods can infer the name of an anonymous function from its syntactic position (new in ECMAScript 2015)
+    const isFunctionAndMatchCondition = typeof action === 'function' && regexActionName.test(action.name) && !connectionState;
+
+    const isObjectAndMatchCondition = typeof action === 'object' && (regexActionType.test(action.type) || actionTypes.includes(action.type)) && !connectionState;
+
+    if (isFunctionAndMatchCondition || isObjectAndMatchCondition) {
+      return next({
+        type: FETCH_OFFLINE_MODE,
+        payload: {
+          prevActionType: action.type,
+          prevActionPayload: action.payload,
+        },
+      });
+    }
+    return next(action);
+  };
+}
+
+export default createNetworkMiddleware;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export reducer from './reducer';
 export withNetwork from './withNetwork';
 export ConnectivityRenderer from './ConnectivityRenderer';
+export createNetworkMiddleware from './createNetworkMiddleware';
