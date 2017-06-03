bc83528e37c547311c2d7e96b645493800b22fe7
Raul Gomez Acuna <rauliyohmc@gmail.com>
2017-06-03 17:51:51 +0200

    version bump + eslint fix

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -11,7 +11,8 @@ const actionTypes: ActionTypes = {
   CONNECTION_CHANGE: '@@network-connectivity/CONNECTION_CHANGE',
   FETCH_OFFLINE_MODE: '@@network-connectivity/FETCH_OFFLINE_MODE',
   REMOVE_FROM_ACTION_QUEUE: '@@network-connectivity/REMOVE_FROM_ACTION_QUEUE',
-  DISMISS_ACTIONS_FROM_QUEUE: '@@network-connectivity/DISMISS_ACTIONS_FROM_QUEUE'
+  DISMISS_ACTIONS_FROM_QUEUE:
+    '@@network-connectivity/DISMISS_ACTIONS_FROM_QUEUE'
 };
 
 export default actionTypes;
