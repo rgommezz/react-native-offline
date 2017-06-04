2699e14bd488ca094089b5de67df5cdecab3d4a7
Krzysztof Borowy <krzysztoff.borowy@gmail.com>
2017-06-04 23:24:47 +0200

    Added timeout setting

--- src/checkInternetAccess.js
+++ src/checkInternetAccess.js
@@ -2,13 +2,43 @@
 
 export default async (
   isConnected: boolean,
-  address: string = 'http://google.com'
+  address: string = 'http://google.com/text.txt', // We don't need a valid respond, but ANY respond
+  timeout: number = 3000
 ) => {
   if (!isConnected) {
     return isConnected;
   }
+  let timedOut = false;
+
+  timeout(() => {
+    timedOut = true;
+  }, timeout);
 
   return new Promise(resolve => {
-    fetch(address).then(() => resolve(true)).catch(() => resolve(false));
+    fetch(address)
+      .then(() => {
+        resolve(!timedOut); // We have internet connection if not timmed out
+      })
+      .catch(() => resolve(false));
   });
 };
+
+/*
+
+export default function makeCancelable(promise: Promise<*>): CancelablePromise {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then((val) => !hasCanceled_ && resolve(val));
+    promise.catch((error) => !hasCanceled_ && reject(error));
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    },
+  };
+}
+
+*/
