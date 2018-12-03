0bff076c0bb5b839198faf045872f820bc2adf36
Fabian Cook <faybecook@gmail.com>
2018-12-03 21:54:56 +1300

    Correct linter

--- src/checkInternetConnection.js
+++ src/checkInternetConnection.js
@@ -39,7 +39,7 @@ export default function checkInternetConnection(
             'connectionChange',
             handleFirstConnectivityChangeIOS,
           );
-        })
+        });
     });
   }
 
