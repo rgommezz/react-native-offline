9e0b30f1e5380e868133ef23edb8e65c17763132
Raul Gomez <rauliyohmc@gmail.com>
2019-01-01 17:49:28 +0100

    Refactoring function

--- src/utils/checkInternetConnection.js
+++ src/utils/checkInternetConnection.js
@@ -1,44 +1,26 @@
 /* @flow */
 
-import { Platform, NetInfo } from 'react-native';
+import { NetInfo } from 'react-native';
 import checkInternetAccess from './checkInternetAccess';
 import { DEFAULT_PING_SERVER_URL, DEFAULT_TIMEOUT } from './constants';
 
 /**
  * Utility that allows to query for internet connectivity on demand
- * On iOS, the listener is fired immediately after registration
- * On Android, we need to use `isConnected.fetch`, that returns a promise which resolves with a boolean
  * @param url
  * @param timeout
+ * @param shouldPing
  * @returns {Promise<boolean>}
  */
-export default function checkInternetConnection(
+export default async function checkInternetConnection(
   url: string = DEFAULT_PING_SERVER_URL,
   timeout: number = DEFAULT_TIMEOUT,
+  shouldPing: boolean = true,
 ): Promise<boolean> {
-  let connectionChecked: Promise<boolean>;
-  if (Platform.OS === 'ios') {
-    connectionChecked = new Promise((resolve: Function) => {
-      const handleFirstConnectivityChangeIOS = (isConnected: boolean) => {
-        NetInfo.isConnected.removeEventListener(
-          'connectionChange',
-          handleFirstConnectivityChangeIOS,
-        );
-        resolve(isConnected);
-      };
-      NetInfo.isConnected.addEventListener(
-        'connectionChange',
-        handleFirstConnectivityChangeIOS,
-      );
-    });
-  } else {
-    connectionChecked = NetInfo.isConnected.fetch();
-  }
-
-  return connectionChecked.then((isConnected: boolean) => {
-    if (isConnected) {
-      return checkInternetAccess({ timeout, url });
+  return NetInfo.isConnected.fetch().then(async (isConnected: boolean) => {
+    if (shouldPing) {
+      const hasInternetAccess = await checkInternetAccess({ timeout, url });
+      return hasInternetAccess;
     }
-    return Promise.resolve(false);
+    return isConnected;
   });
 }
