c3c6e05dd4f8e1f8e1a701b990b6c76a6f0a9446
Raul Gomez <rauliyohmc@gmail.com>
2019-02-03 00:35:14 +0000

    Adding longpress hook for explaining each action

--- example/components/ActionButtons.js
+++ example/components/ActionButtons.js
@@ -1,7 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, View, StyleSheet, Text } from 'react-native';
+import {
+  Alert,
+  View,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 import { addOne, subOne, other, cancelOther } from '../redux/actions';
+
+const Touchable = Platform.select({
+  android: TouchableNativeFeedback,
+  ios: TouchableOpacity,
+});
+
+const showInfo = actionType => () => {
+  const message = (() => {
+    switch (actionType) {
+      case 'ADD_1':
+        return (
+          'This action adds 1 to the counter state. It is configured to be enqueued if offline ' +
+          'and  uses a unique id per action, so that we can add to the queue as many as we want.'
+        );
+      case 'SUB_1':
+        return (
+          'This action subtracts 1 to the counter state. It is configured to be enqueued if offline ' +
+          'and uses a unique id per action, so that we can add to the queue as many as we want.'
+        );
+      case 'OTHER':
+        return (
+          "This action does not change the UI state. It's only purpose is to demonstrate that if we dispatch the same " +
+          'action to the queue several times, it will replace the existing one and a new instance to the end of the queue. ' +
+          "It's also dismissable, so that if an action with type CANCEL_OTHER is dispatched, it will be removed from the queue."
+        );
+      case 'CANCEL_OTHER':
+        return 'This action does not change the UI state. If dispatched, it will dismiss actions from the queue with type OTHER.';
+      default:
+        return '';
+    }
+  })();
+  Alert.alert(actionType, message, [{ text: 'OK', onPress: () => ({}) }], {
+    cancelable: false,
+  });
+};
 
 function ActionButtons({
   addOneAction,
@@ -11,18 +54,39 @@ function ActionButtons({
 }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Actions to dispatch:</Text>
+      <Text style={styles.title}>Actions to dispatch</Text>
+      <Text style={styles.subtitle}>Long tap on each action for more info</Text>
       <View style={styles.row}>
-        <Button onPress={addOneAction} title="ADD_1" color="#388E3C" />
-        <Button onPress={subOneAction} title="SUB_1" color="#388E3C" />
+        <Touchable
+          onPress={addOneAction}
+          onLongPress={showInfo('ADD_1')}
+          style={{ justifyContent: 'center', marginHorizontal: 8 }}
+        >
+          <Text style={styles.button}>ADD_1</Text>
+        </Touchable>
+        <Touchable
+          onPress={subOneAction}
+          onLongPress={showInfo('SUB_1')}
+          style={{ justifyContent: 'center', marginHorizontal: 8 }}
+        >
+          <Text style={styles.button}>SUB_1</Text>
+        </Touchable>
       </View>
       <View style={styles.row}>
-        <Button onPress={otherAction} title="OTHER" color="#388E3C" />
-        <Button
+        <Touchable
+          onPress={otherAction}
+          onLongPress={showInfo('OTHER')}
+          style={{ justifyContent: 'center', marginHorizontal: 8 }}
+        >
+          <Text style={styles.button}>OTHER</Text>
+        </Touchable>
+        <Touchable
           onPress={cancelOtherAction}
-          title="CANCEL_OTHER"
-          color="#388E3C"
-        />
+          onLongPress={showInfo('CANCEL_OTHER')}
+          style={{ justifyContent: 'center', marginHorizontal: 8 }}
+        >
+          <Text style={styles.button}>CANCEL_OTHER</Text>
+        </Touchable>
       </View>
     </View>
   );
@@ -51,8 +115,19 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
   },
+  subtitle: {
+    fontSize: 14,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
   row: {
     justifyContent: 'center',
     flexDirection: 'row',
+    marginVertical: 8,
+  },
+  button: {
+    color: '#388E3C',
+    fontSize: 18,
   },
 });
