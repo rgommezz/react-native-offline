d72760d1acf274ddbb89a2ee3bbc1456d9214239
Ankeet Maini <ankeet.maini@gmail.com>
2020-01-21 07:07:23 +0530

    Fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as offlineActionTypes from './redux/actionTypes';
+import * as offlineActionCreators from './redux/actionCreators';
 import _reducer from './redux/createReducer';
+
 export { default as NetworkProvider } from './components/NetworkProvider';
 export {
   default as ReduxNetworkProvider,
@@ -8,13 +11,13 @@ export { default as NetworkConsumer } from './components/NetworkConsumer';
 export {
   default as createNetworkMiddleware,
 } from './redux/createNetworkMiddleware';
-export * as offlineActionTypes  from './redux/actionTypes';
+
 export { default as networkSaga } from './redux/sagas';
 export {
   default as checkInternetConnection,
 } from './utils/checkInternetConnection';
-export * as offlineActionCreators  from './redux/actionCreators';
 
 export const createReducer = _reducer;
 export const reducer = _reducer();
 
+export { offlineActionCreators, offlineActionTypes };
