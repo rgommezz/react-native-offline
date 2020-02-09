a22be7f8f557551bb3a377714f99361f79b5ed76
rgommezz <rauliyohmc@gmail.com>
2020-02-09 20:54:02 +0000

    using TS version on example

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import AppNavigator from './navigation/AppNavigator';
 import DummyNetworkContext from './DummyNetworkContext';
