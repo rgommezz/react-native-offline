cd0558d59f641fb22450ab577c8028b1d35e6a9c
rgommezz <rauliyohmc@gmail.com>
2021-02-25 19:41:17 +0100

    fix: tests

--- src/components/NetworkContext.tsx
+++ src/components/NetworkContext.tsx
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import { ConnectivityState } from '../types';
 
-const NetworkContext = createContext<ConnectivityState>({ isConnected: true });
+const NetworkContext = createContext<ConnectivityState | undefined>(undefined);
 
 export default NetworkContext;
